7927136a35ab6e2cf31278af4301ab573c74eb9e
gerardo <gamadrid@iaggbs.com>
2025-07-30 15:09:38 +0200

    adjusting for fe displying only

--- src/Frame.tsx
+++ src/Frame.tsx
@@ -1,25 +1,26 @@
-import { useState, useEffect, useRef } from "react";
+import { useRef } from "react";
 import GanttChart from "./GanttChart";
 import Header from "./Header";
-import { links, scales } from "./common/data";
+import { links, scales,basic_tasks } from "./common/data";
 import type { Link, Scale, Task } from "./common/interfaces";
 
 const first_links: Link[] = links;
 const first_scales: Scale[] = scales;
+const tasks:Task[] = basic_tasks;
 
 const Frame = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const apiRef = useRef();
+  // const [tasks, setTasks] = useState<Task[]>([]);
+  const apiRef = useRef(null);
 
-  const getData = async () => {
-    const req = await fetch("http://localhost:3000");
-    const response = await req.json();
-    setTasks(response);
-  };
+  // const getData = async () => {
+  //   const req = await fetch("http://localhost:3000");
+  //   const response = await req.json();
+  //   setTasks(response);
+  // };
 
-  useEffect(() => {
-    getData();
-  }, []);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
 
   return (
     <div className=" bg-gray-400 w-screen h-screen">
